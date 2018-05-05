d96cc5382836475eaeda415d4ed6855cddcae0f2
CheiZhei <139113163@qq.com>
2018-05-05 20:59:46 +0800

    test

---
+++ data.js
@@ -0,0 +1,12 @@
+[
+{
+  title: 'mui开发教程',
+  tag: ['mui', 'javascript', 'plus'],
+  time: '2017-07-31',
+  category: 'mui开发',
+  discribe:'页面传参数是一种比较常见的业务需求，在html5+ app中，根据实现原理及适用环境可以分为两大类。',
+  toname: 'article/category/mui.md',
+  content: '',
+  nav: []
+}
+]
